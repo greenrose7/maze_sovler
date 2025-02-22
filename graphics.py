a476180079de410c2c0a459d5117d24860315eed
greenrose7 <lucascevans@live.com>
2025-02-21 22:01:59 -0400

    testing line drawing

--- graphics.py
+++ graphics.py
@@ -1,11 +1,11 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window():
-    def __init__(self, width, height, title):
+    def __init__(self, width, height, title :str):
         self.__root = Tk()
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__root.title(title)
-        self.canvas = Canvas()
+        self.canvas = Canvas(self.__root, width=width, height=height)
         self.canvas.pack()
         self.running = False
     
@@ -20,3 +20,19 @@ class Window():
     
     def close(self):
         self.running = False
+    
+    def draw_line(self, line, fill_color :str):
+        line.draw(self.canvas, fill_color)
+
+class Point():
+    def __init__(self, x :int, y :int):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point_one :Point, point_two :Point):
+        self.point_one = point_one
+        self.point_two = point_two
+
+    def draw(self, canvas :Canvas, fill_color :str):
+        canvas.create_line(self.point_one.x, self.point_one.y, self.point_two.x, self.point_two.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from graphics import Window
+from graphics import *
 
 def main():
     win = Window(800, 600, "my title")
+    test_lines(win)
     win.wait_for_close()
+
+def test_lines(window :Window):
+    top_left = Point(0,0)
+    bottom_right = Point(800, 600)
+    tl_br_line = Line(top_left, bottom_right)
+    tl_br_line.draw(window.canvas, "red")
+
+    mid_left = Point(0, 300)
+    mid_right = Point(800, 300)
+    ml_mr_line = Line(mid_right, mid_left)
+    ml_mr_line.draw(window.canvas, "purple")
 
 if __name__ == "__main__":
     main()
