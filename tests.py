bd2b0668f4461f74f6d12b8cad9e89002d35e443
greenrose7 <lucascevans@live.com>
2025-02-25 16:50:18 -0400

    added test for enterance and exit

--- tests.py
+++ tests.py
@@ -40,6 +40,20 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+    
+    def test_maze_start_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, None)
+        self.assertTrue(
+            (m1._cells[0][0].has_left_wall or m1._cells[0][0].has_top_wall)
+            and not (m1._cells[0][0].has_left_wall and m1._cells[0][0].has_top_wall)
+        )
+        self.assertTrue(
+            (m1._cells[num_cols-1][num_rows-1].has_right_wall or m1._cells[num_cols-1][num_rows-1].has_bottom_wall)
+            and not (m1._cells[num_cols-1][num_rows-1].has_right_wall and m1._cells[num_cols-1][num_rows-1].has_bottom_wall)
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
